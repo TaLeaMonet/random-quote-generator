fdb6b658fe2962fd9fd7b8f4ba01710f3aebf7ac
TaLea Carpenter <32846833+TaLeaMonet@users.noreply.github.com>
2022-05-23 13:26:24 -0700

    update code with comments.

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -23,6 +23,7 @@ project 1 - A Random Quote Generator
  * `getRandomQuote` function
 ***/
 function getRandomQuote() {
+ //Array storing quotes objects.  
   let quotes = [{quote: 'In times of change, learners inherit the earth, while the learned find themselves beautifully equipped to deal with a world that no longer exists.', source: 'Eric Hoffer'},
               {quote: 'My mission in life is not merely to survive, but to thrive; and to do so with some passion some compassion, some humor and some style.', source: 'Maya Angelou' , citation:'harperbazaar.com', year:2015},
               {quote:'If you want to find the secrets of the universe, think in terms of energy, frequency and vibration.', source:'Nikola Tesla', citation:'goodreads.com'},
@@ -31,6 +32,7 @@ function getRandomQuote() {
               {quote:'Nothing is absolute. Everything changes, everything moves, everything revolves, everything flies and goes away.', source:'Frida Kahlo', citation:'goalcast.com'}, 
               {quote:'Everything negative—pressure, challenges—is all an opportunity for me to rise.', source:'Kobe Bryant', citation:'parade.com', year:2018},
               {quote:'Every great developer you know got there by solving problems they were unqualified to solve until they actually did it.', source:'Patrick McKenzie', citation:'Twitter', year:2016}];
+  //Variable storing logic to randomize quotes.           
   let randomizeQuote = Math.floor(Math.random() * quotes.length);
  return quotes[randomizeQuote];
 }
@@ -41,14 +43,19 @@ function getRandomQuote() {
  * `printQuote` function
 ***/
 function printQuote() {
+  //Variable calling the getRandomQuote function
   let randomQuote = getRandomQuote();
+  //Variable that will display source and quote properties
   let htmlString = `<p class = "quote">${randomQuote.quote}</p> 
-                   <p class = "source">${randomQuote.source}`  
+                   <p class = "source">${randomQuote.source}` 
+
+  //conditional checking if citation property exists                  
   if(randomQuote.citation) {
       htmlString = `<p class = "quote">${randomQuote.quote}</p> 
                    <p class = "source">${randomQuote.source}
                   <span class="citation">${randomQuote.citation}</span>` 
   } 
+  //conditional checking if year property exists
   if(randomQuote.year) {
     htmlString = `<p class = "quote">${randomQuote.quote}</p> 
     <p class = "source">${randomQuote.source}
@@ -56,8 +63,10 @@ function printQuote() {
    <span class="year">${randomQuote.year}</span>` 
   }
 
+//Append closing paragraph to the end of the HTML string 
 htmlString += '</p>'
 
+//Return final HTML string
 return document.getElementById('quote-box').innerHTML = htmlString;
 }
 printQuote();
