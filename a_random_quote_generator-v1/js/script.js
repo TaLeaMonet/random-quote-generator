865b74dbc62dd31cd5484977cba2fbe2ff9bdf47
TaLea Carpenter <32846833+TaLeaMonet@users.noreply.github.com>
2022-05-24 14:45:17 -0700

    update quotes array. work on extra credit prompts.

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -10,33 +10,23 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-// let quotes = [{quote: 'In times of change, learners inherit the earth, while the learned find themselves beautifully equipped to deal with a world that no longer exists.', source: 'Eric Hoffer'},
-//               {quote: 'My mission in life is not merely to survive, but to thrive; and to do so with some passion some compassion, some humor and some style.', source: 'Maya Angelou' , citation:'https://www.harperbazaar.com'},
-//               {quote:'If you want to find the secrets of the universe, think in terms of energy, frequency and vibration.', source:'Nikola Tesla', citation:'https://www.goodreads.com', year:'Unknown'},
-//               {quote:'Reality is wrong. Dreams are for real.', source:'Tupac Shakur', citation:'https://www.quoteambition.com'},
-//               {quote:'I\'ll tell you what freedom is to me: no fear.', source:'Nina Simone', citation:'https://www.kidadl.com', year:'Unknown'}, 
-//               {quote:'Nothing is absolute. Everything changes, everything moves, everything revolves, everythings flies and goes away.', source:'Frida Kahlo', citation:'https://www.goalcast.com'}, 
-//               {quote:'Everything negative—pressure, challenges—is all an opportunity for me to rise.', source:'Kobe Bryant', citation:'https://www.parade.com', year:'Unknown'}];
-
-
+// Array storing quotes objects. 
+let quotes = [{quote: 'In times of change, learners inherit the earth, while the learned find themselves beautifully equipped to deal with a world that no longer exists.', source: 'Eric Hoffer', tag:'inspirational'},
+              {quote: 'My mission in life is not merely to survive, but to thrive; and to do so with some passion some compassion, some humor and some style.', source: 'Maya Angelou' , citation:'harperbazaar.com', year:2015, tag:'inspirational'},
+              {quote:'If you want to find the secrets of the universe, think in terms of energy, frequency and vibration.', source:'Nikola Tesla', citation:'goodreads.com', tag:'theory'},
+              {quote:'Reality is wrong. Dreams are for real.', source:'Tupac Shakur', citation:'quoteambition.com'},
+              {quote:'I\'ll tell you what freedom is to me: no fear.', source:'Nina Simone', citation:'kidadl.com', year:1970}, 
+              {quote:'Nothing is absolute. Everything changes, everything moves, everything revolves, everything flies and goes away.', source:'Frida Kahlo', citation:'goalcast.com'}, 
+              {quote:'Everything negative—pressure, challenges—is all an opportunity for me to rise.', source:'Kobe Bryant', citation:'parade.com', year:2018, tag:'motivational'},
+              {quote:'Every great developer you know got there by solving problems they were unqualified to solve until they actually did it.', source:'Patrick McKenzie', citation:'Twitter', year:2016, tag:'inspirational'}];
 /***
  * `getRandomQuote` function
 ***/
 function getRandomQuote() {
- //Array storing quotes objects.  
-  let quotes = [{quote: 'In times of change, learners inherit the earth, while the learned find themselves beautifully equipped to deal with a world that no longer exists.', source: 'Eric Hoffer'},
-              {quote: 'My mission in life is not merely to survive, but to thrive; and to do so with some passion some compassion, some humor and some style.', source: 'Maya Angelou' , citation:'harperbazaar.com', year:2015},
-              {quote:'If you want to find the secrets of the universe, think in terms of energy, frequency and vibration.', source:'Nikola Tesla', citation:'goodreads.com'},
-              {quote:'Reality is wrong. Dreams are for real.', source:'Tupac Shakur', citation:'quoteambition.com'},
-              {quote:'I\'ll tell you what freedom is to me: no fear.', source:'Nina Simone', citation:'kidadl.com', year:1970}, 
-              {quote:'Nothing is absolute. Everything changes, everything moves, everything revolves, everything flies and goes away.', source:'Frida Kahlo', citation:'goalcast.com'}, 
-              {quote:'Everything negative—pressure, challenges—is all an opportunity for me to rise.', source:'Kobe Bryant', citation:'parade.com', year:2018},
-              {quote:'Every great developer you know got there by solving problems they were unqualified to solve until they actually did it.', source:'Patrick McKenzie', citation:'Twitter', year:2016}];
   //Variable storing logic to randomize quotes.           
   let randomizeQuote = Math.floor(Math.random() * quotes.length);
  return quotes[randomizeQuote];
 }
-
 
 
 /***
@@ -62,6 +52,10 @@ function printQuote() {
     <span class="citation">${randomQuote.citation}</span>
    <span class="year">${randomQuote.year}</span>` 
   }
+  //conditional checking if tag property exists
+  if(randomQuote.tag) {
+    htmlString += `, <span class="tag">${randomQuote.tag}</span>`
+  }
 
 //Append closing paragraph to the end of the HTML string 
 htmlString += '</p>'
@@ -70,6 +64,26 @@ htmlString += '</p>'
 return document.getElementById('quote-box').innerHTML = htmlString;
 }
 printQuote();
+
+
+//Extra Credit!!!
+//1. Random background colors
+//Variable to target the load-quote button
+let bgColor = document.getElementById('load-quote');
+
+//Function using .floor and .random method to randomize background's color using hex values
+function changeBackgroundColor() {
+  let randomColor = Math.floor(Math.random()*16777215).toString(16);
+  document.getElementById("background-color").style.backgroundColor = '#' + randomColor;
+  return randomColor; 
+}
+//Event listener for color randomization 
+bgColor.addEventListener("click", changeBackgroundColor);
+
+//2. Auto-refreshed quotes
+//Using the setInterval method to call the printQuote and changeBackgroundColor functions evey 5 seconds
+setInterval(printQuote, 10000);
+setInterval(changeBackgroundColor, 10000);
 
 
 /***
